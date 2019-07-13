726a1ee12446960f4d13f2e8ec81c6bc37476a4d
Josh Wright <wright.jjw@gmail.com>
2019-07-13 00:48:47 -0500

    added tables and table function

--- src/main/java/inittracker/App.java
+++ src/main/java/inittracker/App.java
@@ -1,17 +1,49 @@
 package inittracker;
 
+import javax.swing.*;
+
 /**
  * Hello world!
  */
 public final class App {
-    private App() {
+    public static void main(String[] args) {
+        App app = new App();
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    int SIZE = 20;
+
+    JFrame mainWindow;
+    JFrame displayWindow;
+    JTable table;
+    InitList inits;
+    Object[][] data;
+
+    private App() {
+        createMainWindow();
+    }
+
+    private void createMainWindow() {
+        mainWindow = new JFrame();
+
+        inits = new InitList();
+        String[] titles = {"Init", "Name", "AC"};
+        table = new JTable(initListToObjectArray(inits), titles);
+        table.setBounds(SIZE*40, SIZE*20, SIZE*60, SIZE*60);
+        mainWindow.add(table);
+
+        mainWindow.setVisible(true);
+        mainWindow.setSize(SIZE*100,SIZE*80);
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+
+    static Object[][] initListToObjectArray(InitList il) {
+        int s = il.getSize();
+        Object[][] o = new Object[s][];
+        for (int i = 0; i < s; i++) {
+            Object[] array = {il.get(i).init, il.get(i).name, il.get(i).ac};
+            o[i] = array;
+        }
+        return o;
     }
 }
--- src/test/java/inittracker/AppTest.java
+++ src/test/java/inittracker/AppTest.java
@@ -8,11 +8,22 @@ import static org.junit.Assert.*;
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test.
-     */
+    
     @Test
-    public void testApp() {
-        assertTrue(true);
+    public void testInitListToObjectArray() {
+        InitList il = new InitList();
+        InitBlock ib1 = new InitBlock("Klaus", 15, 17);
+        InitBlock ib2 = new InitBlock("Remus", 18, 13);
+        il.add(ib1); il.add(ib2);
+
+        Object[][] data = App.initListToObjectArray(il);
+
+        assertEquals("[1,0] is 18", 18, data[0][0]);
+        assertEquals("[1,1] is Remus", "Remus", data[0][1]);
+        assertEquals("[1,2] is 13", 13, data[0][2]);
+
+        assertEquals("[0,0] is 15", 15, data[1][0]);
+        assertEquals("[0,1] is Klaus", "Klaus", data[1][1]);
+        assertEquals("[0,2] is 17", 17, data[1][2]);
     }
 }
