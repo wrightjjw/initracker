7a862542eefc89c0712872cae3adf85e6793a37f
Josh Wright <wright.jjw@gmail.com>
2019-07-14 09:15:14 -0500

    Restructuring

--- src/main/java/inittracker/App.java
+++ src/main/java/inittracker/App.java
@@ -1,20 +1,24 @@
 package inittracker;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 /**
  * Hello world!
  */
-public final class App {
+public final class App implements ActionListener {
     public static void main(String[] args) {
         App app = new App();
     }
 
-    int SIZE = 20;
+    int SIZE = 10;
 
     JFrame mainWindow;
     JFrame displayWindow;
     JTable table;
+    JTextField nameBox;
+    JTextField initBox;
+    JTextField acBox;
     InitList inits;
     Object[][] data;
 
@@ -23,22 +27,64 @@ public final class App {
     }
 
     private void createMainWindow() {
-        mainWindow = new JFrame();
+        mainWindow = new JFrame("Initiative Tracker");
 
         inits = new InitList();
         String[] titles = {"Init", "Name", "AC"};
-        table = new JTable(initListToObjectArray(inits), titles);
-        table.setBounds(SIZE*40, SIZE*20, SIZE*60, SIZE*60);
-        mainWindow.add(table);
 
+        JPanel top = new JPanel();
+        JPanel mid = new JPanel();
+        JPanel bot = new JPanel();
+
+        table = new JTable(initListToObjectArray(inits), titles);
+        mid.add(table);
+
+        JLabel nameLabel = new JLabel("Name:");
+        bot.add(nameLabel);
+
+        nameBox = new JTextField(10);
+        bot.add(nameBox);
+
+        JLabel initLabel = new JLabel("Init:");
+        bot.add(initLabel);
+
+        initBox = new JTextField(2);
+        bot.add(initBox);
+
+        JLabel acLabel = new JLabel("AC:");
+        bot.add(acLabel);
+
+        acBox = new JTextField(2);
+        bot.add(acBox);
+
+        JButton addInitButton = new JButton("Add Entry");
+        bot.add(addInitButton);
+        addInitButton.setActionCommand("add entry");
+        addInitButton.addActionListener(this);
+
+
+        mainWindow.getContentPane().add("North", top);
+        mainWindow.getContentPane().add("Center", mid);
+        mainWindow.getContentPane().add("South", bot);
         mainWindow.setVisible(true);
         mainWindow.setSize(SIZE*100,SIZE*80);
         mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-
-    private void addInitEntry(String name, Integer init, Integer ac) {
-        inits.add(new InitBlock(name, init, ac));
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        if (command == "add entry") {
+            System.out.println("Adding init entry!");
+            String name = nameBox.getText();
+            int init = Integer.parseInt( initBox.getText() );
+            int ac = Integer.parseInt ( acBox.getText() );
+            inits.add(new InitBlock(name, init, ac));
+            data = initListToObjectArray(inits);
+            //TODO: Implement error handling
+        }
+        else {
+            System.out.println("Action performed, but no command recognized.");
+        }
     }
 
     static Object[][] initListToObjectArray(InitList il) {
